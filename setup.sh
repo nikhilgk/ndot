26a18902ad0acd4ff082c95007e9a36a3af3d70b
Nikhil Kurup <ngk@nikhil.ws>
2015-08-19 20:59:36 -0400

    Fixed missing recurisve git setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get -y install zsh git-core stow source-highlight
 cd
-git clone https://github.com/nikhilgk/ndot ~/ndot
+git clone --recursive https://github.com/nikhilgk/ndot ~/ndot
 cd ~/ndot
 ./basic.sh
 chsh -s $(which zsh)
