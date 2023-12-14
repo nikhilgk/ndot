c6a81dbbf30e12b21866d28a2ae372c71e6189ae
Nikhil Kurup <ngk@nikhil.ws>
2023-12-14 11:35:26 -0500

    Tweaks and fixes

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
-rm -rf i3
-rm -rf polybar
-cp -afu ~/.config/i3/ i3
-cp -afu ~/.config/polybar/ polybar
-git add .
-git commit -m "Backing up i3 and polybar"
+# rm -rf i3
+# rm -rf polybar
+# cp -afu ~/.config/i3/ i3
+# cp -afu ~/.config/polybar/ polybar
+# git add .
+# git commit -m "Backing up i3 and polybar"
