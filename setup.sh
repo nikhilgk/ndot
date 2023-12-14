2848166f134d3b0a2fc2b40cadef16074ab78630
Nikhil Kurup <ngk@nikhil.ws>
2023-12-14 11:00:01 -0500

    Tweaks and fixes

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt-get -y install zsh git-core stow source-highlight nano rename
 cd
 git clone --recursive https://github.com/nikhilgk/ndot ~/ndot
 cd ~/ndot
-wget -qO- https://foicica.com/textadept/download/textadept_LATEST.x86_64.tgz | tar xvzf - -C ~/ndot
+wget -qO- https://github.com/orbitalquark/textadept/releases/download/textadept_11.4/textadept_11.4.linux.tgz | tar xvzf - -C ~/ndot
 rename s/_..\*// textadept*
 sudo ln -s $HOME/ndot/textadept/textadept-curses /usr/bin/nedit
 ./basic.sh
